dfb7833083ff0c41900f651a36c71a10c6c7d655
Florent <florent.hagard@gmail.com>
2020-05-02 18:04:11 +0200

    auto-commit

--- application/bootloader.js
+++ application/bootloader.js
@@ -1,2 +1,14 @@
-console.log('hello docker')
-console.log(process.env.NODE_ENV);
+const http    = require('http');
+const socket  = require('socket.io');
+
+http.createServer((req, res) => {
+	console.log(req);
+}).listen(3000,() => {
+	console.log('listening on *:3000');
+});
+
+/*
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+*/
