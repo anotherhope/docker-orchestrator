9f62c4dda04914aadd8222c2535017c2c8584351
Florent <florent.hagard@gmail.com>
2020-05-07 00:34:04 +0200

    auto-commit

--- application/lib/runner.js
+++ application/lib/runner.js
@@ -1,10 +1,34 @@
-const Parser 	= require(__dirname + "/parser.js");
+const Parser    = require(__dirname + "/parser.js");
+const DockerAPI = require("dockerode");
+const api 		= new DockerAPI({ 
+	socketPath: process.env.DOCKER_SOCKET || '/var/run/docker.sock'
+});
 
-module.exports = class Runner {
+module.exports  = class Runner {
 
 	static deploy(path = null){
 		let compose = Parser.loadFrom(path);
 
-		console.log(compose);
+		for (let service of compose.services){
+			
+			console.log(service);
+			/*
+			DockerAPI.buildImage({
+				context: __dirname,
+				src: ['Dockerfile', 'file1', 'file2']
+			},{
+				t: "imageName"
+			}, function (err, response) {
+				console.log(...arguments);
+			});
+			*/
+
+		}
+
+
+
+		//console.log(compose);
 	}
+
+
 }
