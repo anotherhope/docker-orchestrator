1b21fbe728e83443746974c434ca24d215ed7905
Florent <florent.hagard@gmail.com>
2020-05-07 01:19:46 +0200

    auto-commit

--- application/lib/runner.js
+++ application/lib/runner.js
@@ -21,7 +21,7 @@ module.exports  = class Runner {
 				src : [ '../../dockerfiles/bases/' + serviceName ]
 			},{
 				t: serviceName,
-				//dockerfiles : serviceName
+				dockerfile : serviceName
 			}, function (err, response) {
 				console.log(...arguments);
 			});
