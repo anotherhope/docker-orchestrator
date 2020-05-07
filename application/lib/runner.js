2ee4307625d978c3e30c00cbc86f19a6aca50a97
Florent <florent.hagard@gmail.com>
2020-05-07 14:42:52 +0200

    auto-commit

--- application/lib/runner.js
+++ application/lib/runner.js
@@ -2,6 +2,7 @@ const Parser    = require(__dirname + "/parser.js");
 const DockerAPI = require("dockerode");
 const fs 		= require("fs-extra");
 const path 		= require("path");
+const readline 	= require("readline");
 const api 		= new DockerAPI({ 
 	//socketPath: process.env.DOCKER_SOCKET || '/var/run/docker.sock'
 	host: process.env.DOCKER_HOST || '192.168.1.4',
@@ -12,60 +13,64 @@ const api 		= new DockerAPI({
 module.exports  = class Runner {
 
 	static prepare(composePath){
-		let compose    = Parser.loadFrom(composePath);
-		let buildOrder = [];
+		let compose  = Parser.loadFrom(composePath);
+		let services = [];
 
 		for (let serviceName in compose.services){
-			let service = compose.services[serviceName];
-			let context = path.resolve(path.dirname(composePath),service.build.context);
-				fs.mkdirp('/tmp/.build/' + serviceName);
-				fs.emptyDirSync('/tmp/.build/' + serviceName);
-				fs.copySync(context, '/tmp/.build/' + serviceName);
-				fs.copySync(path.resolve(context,service.build.dockerfile), '/tmp/.build/' + serviceName + '/Dockerfile');
+			services.push(
+				new Promise((resolve,reject) => {
+					let service = compose.services[serviceName];
+					let context = path.resolve(path.dirname(composePath),service.build.context);
+						fs.mkdirp('/tmp/.build/' + serviceName);
+						fs.emptyDirSync('/tmp/.build/' + serviceName);
+						fs.copySync(context, '/tmp/.build/' + serviceName);
+						fs.copySync(path.resolve(context,service.build.dockerfile), '/tmp/.build/' + serviceName + '/Dockerfile');
 
-			const lineReader = require('readline').createInterface({
-				input: fs.createReadStream('/tmp/.build/' + serviceName + '/Dockerfile'),
-			}).on('line', (line) => {
-				let match = /FROM\s([a-z0-9_]+)/gi.exec(line);
-				if (match && match[1]){
-					service.from = match[1];
-					lineReader.close();
-				}
-			});
-			console.log(service.from);
+					const lineReader = readline.createInterface({
+						input: fs.createReadStream('/tmp/.build/' + serviceName + '/Dockerfile')
+					}).on('line', (line) => {
+						let match = /FROM\s([a-z0-9_]+)/gi.exec(line);
+						if (match && match[1]){
+							service.from = match[1];
+							lineReader.close();
+						}
+					}).on('close',() => {
+						resolve(service);
+					});
+				})
+			)
 
-
-			service.serviceName = serviceName;
-
-			buildOrder.push(service);
 		}
 
-		console.log(buildOrder);
-
-		return buildOrder;
+		return Promise.all(services);
 	}
 
 	static deploy(composePath){
-		for (let serviceName of this.prepare(composePath)){
 
-			/*
-				api.buildImage({ context: '/tmp/.build/' + serviceName },{
-					t: 'local_' + serviceName + "_image"
-				}, (err, response) => {
-					if (!err){
-						response.on('data', (chunk) => {
-							try {
-								process.stdout.write(JSON.parse(chunk).stream);
-							} catch(e) {
-								process.stdout.write(chunk);
-							}
-						}).on('end',() => {
-							fs.removeSync('/tmp/.build/' + serviceName)
-						});
-					}
-				});
-			*/
-		}
+		this.prepare(composePath).then((services) => {
+
+			for (let serviceName of services){
+
+				/*
+					api.buildImage({ context: '/tmp/.build/' + serviceName },{
+						t: 'local_' + serviceName + "_image"
+					}, (err, response) => {
+						if (!err){
+							response.on('data', (chunk) => {
+								try {
+									process.stdout.write(JSON.parse(chunk).stream);
+								} catch(e) {
+									process.stdout.write(chunk);
+								}
+							}).on('end',() => {
+								fs.removeSync('/tmp/.build/' + serviceName)
+							});
+						}
+					});
+				*/
+			}
+		})
+
 	}
 
 
