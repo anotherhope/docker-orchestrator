c77b762a40cc47e2a603eb165da46d90e458f224
Florent <florent.hagard@gmail.com>
2020-05-07 16:00:46 +0200

    auto-commit

--- application/lib/runner.js
+++ application/lib/runner.js
@@ -56,7 +56,7 @@ module.exports  = class Runner {
 			for (let service of services){
 				api.getImages(service.from).then( response => {
 					response.on('data', (chunk) => {
-						console.log(chunk);
+						console.log(chunk.toString());
 						/*
 						try {
 							process.stdout.write(JSON.parse(chunk).stream);
