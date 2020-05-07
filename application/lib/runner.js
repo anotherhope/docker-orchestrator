84f9ca0d202d118285c823ae937e1cd697b8cb69
Florent <florent.hagard@gmail.com>
2020-05-07 14:14:39 +0200

    auto-commit

--- application/lib/runner.js
+++ application/lib/runner.js
@@ -26,13 +26,13 @@ module.exports  = class Runner {
 			const lineReader = require('readline').createInterface({
 				input: fs.createReadStream('/tmp/.build/' + serviceName + '/Dockerfile'),
 			}).on('line', (line) => {
-				let match = line.match(/FROM\s(a-zA-Z0-9_+)/gi)
+				let match = line.match(/FROM\s([a-z0-9_]+)/gi);
 				if (match){
 					console.log('>>',match);
 					//lineReader.close();
 					//service.from = match[0]
 				}
-				console.log(line);
+				//console.log(line);
 			});
 
 
