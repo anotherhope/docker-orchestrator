57309807021f145e00e45ea40998de8940595dd6
Florent <florent.hagard@gmail.com>
2020-05-09 01:39:19 +0200

    auto-commit

--- application/lib/runner.js
+++ application/lib/runner.js
@@ -49,12 +49,12 @@ module.exports  = class Runner {
 	static retry(fctToRetry, retryUntil = true){
 		if(typeof retryUntil === 'function'){
 			fctToRetry()
-				.then(function()  { return retryUntil({ ...arguments }, true)  ? { ...arguments } : this.retry(fctToRetry, retryUntil); })
-				.catch(function() { return retryUntil({ ...arguments }, false) ? { ...arguments } : this.retry(fctToRetry, retryUntil); })
+				.then(function()  { return retryUntil({ ...arguments }, true)  ? { ...arguments } : Runner.retry(fctToRetry, retryUntil); })
+				.catch(function() { return retryUntil({ ...arguments }, false) ? { ...arguments } : Runner.retry(fctToRetry, retryUntil); })
 		} else {
 			fctToRetry()
-				.then(function()  { return  retryUntil ? { ...arguments } : this.retry(fctToRetry, retryUntil); })
-				.catch(function() { return !retryUntil ? { ...arguments } : this.retry(fctToRetry, retryUntil); })
+				.then(function()  { return  retryUntil ? { ...arguments } : Runner.retry(fctToRetry, retryUntil); })
+				.catch(function() { return !retryUntil ? { ...arguments } : Runner.retry(fctToRetry, retryUntil); })
 		}
 	}
 
@@ -64,6 +64,7 @@ module.exports  = class Runner {
 			if (service.from.match(/^host_/gi)){
 
 				if (services.find( s => 'host_' + s.name === service.from)){
+
 					this.retry( api.getImage( service.from ).get,(data,statment) => { return statment }).then( response => {
 						console.log(response);
 						/*
